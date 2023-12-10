fcdb125eee15eb0a3de7581ee4fcfa87a5fc21d1
nitish <imnitish@yahoo.com>
2023-12-10 14:06:41 +0530

    Add express.json middleware and @types/node as devDependency

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import express from 'express';
+
+class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(port: number) {
+    this.app = express();
+    this.port = port;
+
+    this.initializeMiddlewares();
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(express.json());
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+}
+
+export default App;
