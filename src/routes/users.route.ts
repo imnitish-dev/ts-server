e8ba04d6153dd78939546f5d41441d7c24df0c8b
nitish <imnitish@yahoo.com>
2023-12-11 02:00:32 +0530

    Add user creation and retrieval functionality

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@/interfaces/routes';
 import { Router } from 'express';
+import { Routes } from '@/interfaces/routes';
+import userService from '@/services/UserService';
+import { IUser } from '@/models/UserModel';
 
 export class UserRoute implements Routes {
-  public path:string;
+  public path: string;
   public router = Router();
 
   constructor(path: string) {
@@ -11,8 +13,26 @@ export class UserRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, (_req, res) => {
-      res.send('user');
+    this.router.get(`${this.path}`, async (_req, res) => {
+      const data: IUser = {
+        username: 'Nitish',
+        email: 'nitish@fjfj.com',
+        password: '123456',
+      };
+
+      const newUser = await userService.create(data);
+      console.log(newUser?.toJSON());
+      const user = await userService.getAll(
+        {
+          email: 'Nitish',
+        },
+        {
+          projection: {
+            _id: false,
+          },
+        }
+      );
+      res.send(user);
     });
   }
 }
