821d56084ed519899f0db5700d6f7d1e5705ea2b
nitish <imnitish@yahoo.com>
2023-12-13 01:10:31 +0530

    feat: added somethinf

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'package.json': ['sort-package-json'],
-  '*.{ts,tsx}': ['eslint --max-warnings=0 . --ext=.ts --fix'],
-  '**/*.ts?(x)': () => 'tsc -p tsconfig.json',
+  // 'package.json': ['sort-package-json'],
+  '*.{ts,tsx}': ['npm run lint'],
+  // '**/*.ts?(x)': () => 'tsc -p tsconfig.json',
 };
