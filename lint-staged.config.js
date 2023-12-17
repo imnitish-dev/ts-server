a583aa1c75b00adee66766fc6b84472bcfa4b09c
nitish <imnitish@yahoo.com>
2023-12-17 12:04:56 +0530

    style: prettier
    addded format in lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   'package.json': ['sort-package-json'],
   '*.{ts,tsx}': ['npm run lint'],
+  '*.{ts,tsx}': ['npm run format'],
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json',
 };
